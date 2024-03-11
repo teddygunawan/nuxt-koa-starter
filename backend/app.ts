cb48afbbbf6df63c6c7dcd530e7d4fc47aadbcf5
teddy gunawan <teddygunawan55@gmail.com>
2024-03-11 12:43:24 +0700

    feat: give example for cors settings

--- backend/app.ts
+++ backend/app.ts
@@ -2,9 +2,25 @@
 
 import Koa from "koa";
 import cors from "@koa/cors";
+import dotenv from "dotenv";
 
 const app = new Koa();
-app.use(cors());
+dotenv.config();
+
+// Update your CORS settings here
+const WHITELIST_ORIGIN = new Set(process.env.ACCESS_CONTROL_WHITELIST_ORIGIN?.split(","));
+app.use(
+  cors({
+    origin: function (ctx) {
+      const origin = ctx.header.origin || "";
+      if (WHITELIST_ORIGIN.has(origin) || !origin) {
+        return origin;
+      }
+
+      return "";
+    },
+  })
+);
 
 app.use((ctx) => {
   ctx.body = "Hello World";
